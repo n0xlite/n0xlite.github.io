524aa7e64d8d3c62587d20c371fc86888cbf0430
n0xlite <n0xlite@protonmail.com>
2022-05-25 14:50:01 -0500

    updates

--- src/Total.js
+++ src/Total.js
@@ -2,9 +2,9 @@ export default function Total(props) {
     return (
         <div className="total">
             <h2 className="total--text">Total In/Out:</h2>
-            <h2 className="total--output">${(props.windowPrice + props.screenPrice).toFixed(2)}</h2>
+            <span className="total--output">${(props.windowPrice + props.screenPrice).toFixed(2)}</span>
             <h2 className="total--text">Total Out Only:</h2>
-            <h2 className="total--output">${((props.windowPrice * 0.67) + props.screenPrice).toFixed(2)}</h2>
+            <span className="total--output">${((props.windowPrice * 0.67) + props.screenPrice).toFixed(2)}</span>
         </div>
     )
 }
