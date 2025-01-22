e15abcb24c3b5855fe7471366ea5b1cf46ccc35a
n0xlite <n0xlite@n0xlite.xyz>
2025-01-22 00:52:53 -0600

    pretty icon

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,18 +2,19 @@ import type { MetadataRoute } from 'next'
  
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Next.js App',
-    short_name: 'Next.js App',
-    description: 'Next.js App',
+    name: 'Gwyndows Bid Calculator',
+    short_name: 'bidonsite.xyz',
+    description: 'Bid jobs hella fast',
     start_url: '/',
     display: 'standalone',
     background_color: '#fff',
     theme_color: '#fff',
     icons: [
       {
-        src: '/favicon.ico',
+        src: '/favicon.svg',
         sizes: 'any',
-        type: 'image/x-icon',
+        type: 'svg+xml',
+        purpose: 'any'
       },
     ],
   }
