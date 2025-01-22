bdf74bda288cd964b25d3834a16415662e292476
n0xlite <n0xlite@n0xlite.xyz>
2025-01-21 23:55:54 -0600

    fixed next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',  // Enable static exports
-    images: {
-      unoptimized: true,  // Required for static export
-    },
-    basePath: '/n0xlite.github.io',  // Replace with your repository name
-  }
-  
-  module.exports = nextConfig
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/n0xlite.github.io',
+}
+
+export default nextConfig
