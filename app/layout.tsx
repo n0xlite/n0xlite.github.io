86f02e162110f95ac3c30a2e61c800372a2de461
n0xlite <n0xlite@n0xlite.xyz>
2025-01-22 09:15:33 -0600

    added shortcut and apple to layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,8 @@ export const metadata: Metadata = {
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
+    shortcut: '/favicon.svg',
+    apple: '/favicon.svg'
   },
 };
 
